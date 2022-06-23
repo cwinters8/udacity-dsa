5df0262a669691b8a783baa37d8fcd5eb32edbc8
cwinters8 <clwinters8@gmail.com>
2022-06-22 21:13:18 -0500

    count unique numbers from both files

--- P0/Task1.py
+++ P0/Task1.py
@@ -3,11 +3,12 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files.
 """
 import csv
-with open('texts.csv', 'r') as f:
+
+with open("texts.csv", "r") as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open("calls.csv", "r") as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -18,3 +19,22 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+# helper function that appends to a list if item not in list already
+def append(item, list: list):
+    if item not in list:
+        list.append(item)
+
+
+# put nums in a list if they are not already in the list
+nums = []
+count = 0
+for text in texts:
+    append(text[0], nums)
+    append(text[1], nums)
+
+for call in calls:
+    append(call[0], nums)
+    append(call[1], nums)
+
+# count numbers
+print("There are", len(nums), "different telephone numbers in the records.")
