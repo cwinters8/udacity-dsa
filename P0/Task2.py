106e90539a60748b1e7f200b81e2b7944b28d098
cwinters8 <clwinters8@gmail.com>
2022-06-23 21:21:53 -0500

    unoptimized solution to getting the phone number who spent the longest time on the phone

--- P0/Task2.py
+++ P0/Task2.py
@@ -3,11 +3,12 @@ Read file into texts and calls.
 It's ok if you don't understand how to read files
 """
 import csv
-with open('texts.csv', 'r') as f:
+
+with open("texts.csv", "r") as f:
     reader = csv.reader(f)
     texts = list(reader)
 
-with open('calls.csv', 'r') as f:
+with open("calls.csv", "r") as f:
     reader = csv.reader(f)
     calls = list(reader)
 
@@ -19,4 +20,24 @@ Print a message:
 "<telephone number> spent the longest time, <total time> seconds, on the phone during 
 September 2016.".
 """
+durations_by_phone = {}
+for call in calls:
+    sender = call[0]
+    receiver = call[1]
+    duration = int(call[3])
+    if sender in durations_by_phone.keys():
+        durations_by_phone[sender] += duration
+    else:
+        durations_by_phone[sender] = duration
+    if receiver in durations_by_phone.keys():
+        durations_by_phone[receiver] += duration
+    else:
+        durations_by_phone[receiver] = duration
 
+longest = max(durations_by_phone, key=durations_by_phone.get)
+print(
+    longest,
+    "spent the longest time,",
+    durations_by_phone[longest],
+    "seconds, on the phone during September 2016.",
+)
